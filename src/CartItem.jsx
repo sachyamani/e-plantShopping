b0a5966b142ab1b5dca34744cf9fbc3786c5c143
sachyamani <sach.yamani@gmail.com>
2025-03-16 07:01:37 -0400

    Task3 commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,46 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total=0;
+    cart.forEach((item) => {
+        //extract quantity and cost
+        const unitPrice=parseFloat(item.cost.substring(1)); //to convert string like $10.00 into numbers
+        total += unitPrice * item.quantity; // Multiply cost by quantity and add to total
+    });
+    return total; // Return the total sum
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault(); // Prevent default behavior
+    onContinueShopping(e); // Call the parent component's function
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+      } else {
+        dispatch(removeItem(item.name)); // Remove item when quantity reaches 0
+      } 
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const unitPrice = parseFloat(item.cost.substring(1)); // Extract numeric value from cost string
+    return item.quantity * unitPrice;
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
